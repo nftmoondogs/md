cfd47aa179ab418f27ec3aa85a619d892f8be7e1
nftmoondogs <113340698+nftmoondogs@users.noreply.github.com>
2022-09-14 21:13:21 +0530

    for you vip

--- pages/api/generate-mint-signature.ts
+++ pages/api/generate-mint-signature.ts
@@ -22,7 +22,7 @@ export default async function generateMintSignature(
   const allowList = ["0x7dE5f242a619191dd7E67e0A7cF", "0x7dE5f242a6191dd7E67e0A7cF", "0x3a065dC9d102FBaFdD85e73102CE2b0135355136"]
   const balance = await moondogsnfts.balanceOf(address)
   const tl = await total.totalSupply()
-  const addressInAllowlist = allowList.includes(address) && tl.toNumber() < 1 && balance.toNumber() == 0
+  const addressInAllowlist = allowList.includes(address) && tl.toNumber() < 2 && balance.toNumber() == 0
   const userHasToken = tl.toNumber() < 2 && balance.toNumber() == 1 || balance.toNumber() == 5  
  
 
